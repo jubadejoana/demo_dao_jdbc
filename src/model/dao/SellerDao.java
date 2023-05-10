be652200d52a9061446d571238497af73e20654f
jubadejoana <jubadejjoana@gmail.com>
2023-05-10 15:52:12 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+
+	void insert(Seller obj); //insere o obj no banco de dados//
+	void update(Seller obj); //atualiza o obj no banco de dados//
+	void deleteById(Integer id); //deleta o id//
+	Seller findId(Integer id); //consulta o id//
+	List<Seller> findAll();  //retorna todos os departamentos//
+}
