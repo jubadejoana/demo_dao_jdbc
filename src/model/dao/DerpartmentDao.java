86012573e486975a2ab85c7d85bbe37f1bb362d1
jubadejoana <jubadejjoana@gmail.com>
2023-05-11 16:06:38 -0300

    findById implementation

--- src/model/dao/DerpartmentDao.java
+++ src/model/dao/DerpartmentDao.java
@@ -1,5 +1,15 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DerpartmentDao {
 
+	void insert(Department obj); //insere o obj no banco de dados//
+	void update(Department obj); //atualiza o obj no banco de dados//
+	void deleteById(Integer id); //deleta o id//
+	Department findId(Integer id); //consulta o id//
+	List<Department> findAll();  //retorna todos os departamentos//
+	
 }
